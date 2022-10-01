d15482c54b346b92801b666cf3e23ffd2a33240a
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-10-01 12:59:19 -0400

    Added doxygen comments and TO DO

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,26 @@
+/**
+ * @copyright Copyright (c) 2022
+ *
+ * @file main.cpp
+ * @author Tanuj Thakkar (tanuj@umd.edu)
+ * @version 0.1
+ * @date 2022-10-01
+ * 
+ * @brief ENPM808X, Week 4, Test Driven Development
+ * 
+ * @section DESCRIPTION
+ * 
+ * Main file for PID Controller implementation based on Test Driven Development
+ *
+ */
+
 #include <iostream>
 
-int main()
-{
+int main() {
+    /*
+      TO DO
+      1. Take user input for P, I, and D gain parameters for the PID controller.
+      2. Create a PID instance and call the compute method
+    */
     return 0;
 }
