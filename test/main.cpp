90a31f58b88f9063cb9be61ec693c307ec775f78
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-10-01 12:59:31 -0400

    Added Doxygen comments

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,19 @@
+/**
+ * @copyright Copyright (c) 2022
+ *
+ * @file main.cpp
+ * @author Tanuj Thakkar (tanuj@umd.edu)
+ * @version 0.1
+ * @date 2022-10-01
+ * 
+ * @brief ENPM808X, Week 4, Test Driven Development
+ * 
+ * @section DESCRIPTION
+ *
+ * Main file for testing PID package
+ *
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
