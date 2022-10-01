355f54025c5fab15778cccd0e11028fa79d7325d
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-10-01 12:58:18 -0400

    Added test cases for PID class
    Added test cases to test computeVel and getSamplingRate methods of the PID class

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,49 @@
-#include <gtest/gtest.h>
+/**
+ * @copyright Copyright (c) 2022
+ *
+ * @file test.cpp
+ * @author Tanuj Thakkar (tanuj@umd.edu)
+ * @version 0.1
+ * @date 2022-10-01
+ * 
+ * @brief ENPM808X, Week 4, Test Driven Development
+ * 
+ * @section DESCRIPTION
+ *
+ * This is a test file for PID class.
+ *
+ * Test Cases:
+ * 
+ * Case 1:
+ * Description: It tests the computeVel method of the PID class. The method is
+ * expected to compute the new velocity given the actualy velocity and the 
+ * target velocity of the system.
+ * Expected Result: PASS
+ * 
+ * Case 2:
+ * Description: It tests the getSamplingRate method of the PID class. The method
+ * is expected to return the sampling rate of the PID object.
+ * Expected Result: PASS
+ */
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include <gtest/gtest.h>
+#include "pid.hpp"
+
+
+/**
+ * @brief Construct a new TEST object and implement Test Case 1
+ * 
+ */
+TEST(TestPID, test_computeVel) {
+  PID pid(1.0, 0.1, 0.3, 1.0);
+  EXPECT_EQ(5.75, pid.computeVel(5, 10));
+}
+
+/**
+ * @brief Construct a new TEST object and implement Test Case 2
+ * 
+ */
+TEST(TestPID, test_getSamplingRate) {
+  PID pid(1.0, 0.3, 0.3, 1.0);
+  EXPECT_EQ(1.0, pid.getSamplingRate());
 }
