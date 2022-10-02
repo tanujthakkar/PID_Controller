555491243e9881d6ea14ec1d56e4d21d6b1064e3
Sharmitha Ganesan M.Eng (Robotics) <sharmithaganesan08@gmail.com>
2022-10-02 09:16:23 -0400

    Part 2 update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -2,32 +2,35 @@
  * @copyright Copyright (c) 2022
  *
  * @file main.cpp
- * @author Tanuj Thakkar (tanuj@umd.edu)
+ * @author Part1: Tanuj Thakkar(tanuj@umd.edu) Part 2: Sharmitha
+ * Ganesan(sganesa3@terpmail.umd.edu) Part 2 Driver: Sharmitha Ganesan,
+ * Navigator: Aman Sharma
  * @version 0.1
  * @date 2022-10-01
- * 
+ *
  * @brief ENPM808X, Week 4, Test Driven Development
- * 
+ *
  * @section DESCRIPTION
- * 
+ *
  * Main file for PID Controller implementation based on Test Driven Development
  *
  */
-
-#include <iostream>
+#include "pid.hpp"
 
 int main() {
-    /*
-      TO DO
-      1. Take user input for P, I, and D gain parameters and sampling rate for the PID controller.
-      2. Create a PID instance and call the compute method
-    */
-    cout<<"Enter Kp,Ki,Kd,dt\n";
-    cin>>Kp>>Ki>>Kd>>dt;
-    cout<<"Enter target and actual velocity\n";
-    cin>>target>>actual;
-    PID obj;
-    calculatedVel = obj.computeVel(target,actual);
-        
-    return calculatedVel;
+  /*
+    TO DO
+    1. Take user input for P, I, and D gain parameters and sampling rate for the
+    PID controller.
+    2. Create a PID instance and call the compute method
+  */
+  double Kp, Ki, Kd, dt;
+  cout << "Enter Kp,Ki,Kd,dt\n";
+  cin >> Kp >> Ki >> Kd >> dt;
+  PID obj(Kp, Ki, Kd, dt);
+  double target, actual;
+  cout << "Enter actual and target velocity\n";
+  cin >> target >> actual;
+  obj.computeVel(actual, target);
+  return 0;
 }
